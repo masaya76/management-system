fab84ccf85150582d2b13b32a26092d0a94d3b01
EC2 Default User <ec2-user@ip-172-31-90-254.ec2.internal>
2022-03-23 13:19:17 +0000

    [update]devise：feild_workers コントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :field_workers
   devise_for :members
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
